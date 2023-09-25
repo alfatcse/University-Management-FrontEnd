cae099ab5137812d00341926414a8760aaefdbba
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-09-25 18:43:50 +0200

    redux set up done

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const error = () => {
+  return (
+    <div>
+      <h1>Some thing went wrong</h1>
+    </div>
+  );
+};
+
+export default error;
---
+++ src/app/lib/Providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { store } from "@/app/redux/store";
+import React from "react";
+import { Provider } from "react-redux";
+import StyledComponentsRegistry from "./AntdRegistry";
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </Provider>
+  );
+};
+
+export default Providers;
