62ff0ac18c4f8c1bdc15e3b8f78a329939f1b71b
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-09-30 14:21:59 +0200

    role based dashbar done

---
+++ src/components/ui/Sidebar.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { useState } from "react";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
+const { Header, Content, Footer, Sider } = Layout;
+import { sidebarItems } from "@/constants/sidebaritems";
+import { USER_ROLE } from "@/constants/role";
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const role = USER_ROLE.ADMIN;
+  return (
+    <Sider
+      collapsible
+      collapsed={collapsed}
+      onCollapse={(value) => setCollapsed(value)}
+      width={280}
+      style={{
+        overflow: "auto",
+        height: "100vh",
+        position: "sticky",
+        left: 0,
+        top: 0,
+        bottom: 0,
+      }}
+    >
+      <div
+        style={{
+          color: "white",
+          fontSize: "2rem",
+          textAlign: "center",
+          fontWeight: "bold",
+          marginBottom: "1rem",
+        }}
+        className="demo-logo-vertical"
+      >
+        PH-University
+      </div>
+      <Menu
+        theme="dark"
+        defaultSelectedKeys={["1"]}
+        mode="inline"
+        items={sidebarItems(role)}
+      />
+    </Sider>
+  );
+};
+
+export default Sidebar;
