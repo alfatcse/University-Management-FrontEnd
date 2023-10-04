d5abf6a0f719e18f58804b9d4bfb73ad0f6474c9
Alfat Jahan Rony <34067640+alfatcse@users.noreply.github.com>
2023-10-04 17:31:56 +0200

    Role based dashboard created

---
+++ src/app/(withlayout)/super_admin/change-password/page.tsx
@@ -0,0 +1,5 @@
+const ResetPassPage = () => {
+  return <div></div>;
+};
+
+export default ResetPassPage;
--- src/constants/sidebaritems.tsx
+++ src/constants/sidebaritems.tsx
@@ -19,7 +19,7 @@ export const sidebarItems = (role: string) => {
       children: [
         {
           label: <Link href={`/${role}`}>Account Profile</Link>,
-          key: `/${role}/profile`,
+          key: `/${role}`,
         },
         {
           label: <Link href={`/${role}/change-password`}>Change Password</Link>,
